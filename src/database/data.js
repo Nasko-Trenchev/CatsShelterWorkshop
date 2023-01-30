933c46fd98a8dec6484e7ecac2d5bc2e72831b67
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-01-30 16:58:09 +0200

    Update data.js

--- src/database/data.js
+++ src/database/data.js
@@ -5,10 +5,6 @@ async function main(){
     mongoose.set("strictQuery", false);
 
     await mongoose.connect('mongodb:/127.0.0.1:27017/catShelter');
-
-    async function saveCat(name, imageUrl, breed, description){
-
-    }
 }
 
 async function saveCat(name, imageUrl, breed, description){
